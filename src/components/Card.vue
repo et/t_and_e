db2ba2004079aae6155a085473a2faad824b3af5
Eric Thomas <eric.l.m.thomas@gmail.com>
2018-07-23 08:52:42 -0600

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="card">
+  <div class="card-content">
+    <div class="media-content">
+      <p class="title is-4">
+        <a :href="url">{{ title }}</a>
+      </p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    title: String,
+    url: String
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="section" v-for="section in sections" :key="section.title">
+      <div class="container">
+        <h1 class="title">{{section.title}}</h1>
+        <div class="columns is-centered">
+          <div class="column" v-for="portal in section.portals" :key="portal.url">
+            <Card :title="portal.title" :url="portal.url" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Card from '@/components/Card'
+
+export default {
+  name: 'Home',
+  components: {
+    Card
+  },
+  data: function () {
+    return {
+      sections: [
+        {
+          title: 'Apartment',
+          portals: [
+            {
+              title: 'Maintenance Request',
+              url: 'https://www.ascendatredrocks.com/apartments/co/lakewood/maintenance-request'
+            },
+            {
+              title: 'Pay Rent',
+              url: 'https://domuso.com'
+            }
+          ]
+        },
+        {
+          title: 'Entertainment',
+          portals: [
+            {
+              title: 'Netflix',
+              url: 'https://www.netflix.com/browse'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
